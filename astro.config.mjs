2c4fe1272cb37e594991d2b6f96fec15668536d6
Jesse Pence <jessepence@gmail.com>
2023-10-26 14:30:05 -0700

    my first React project no longer uses React lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config"
 import UnoCSS from "@unocss/astro"
-import react from "@astrojs/react"
 
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
-    react(),
   ],
   markdown: {
     shikiConfig: {
