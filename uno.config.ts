d3aa4388e20e4941ba7db4b2a62876abc57fb602
Jesse Pence <jessepence@gmail.com>
2023-10-26 16:51:39 -0700

    lol whoops

--- uno.config.ts
+++ uno.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     presetWebFonts({
       provider: "google",
       fonts: {
-        sans: ["Pathway Gothic One", "Signika Negative", "IBM Plex Sans"],
         serif: ["JetBrains Mono"],
       },
     }),
